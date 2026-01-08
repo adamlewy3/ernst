77089beb98b95da08f2cf3edd54a7410a17eb263
adamlewy3 <adaskolewandowski6@gmail.com>
2026-01-08 19:48:09 +0000

    minor changes

--- fields/complex.py
+++ fields/complex.py
@@ -6,29 +6,32 @@ class Complex:
     def __add__(self, other):
         return Complex(self.re + other.re, self.im + other.im)        
     
-    def __radd__(self, other):
-        pass
+    def __radd__(self, x):
+        assert isinstance(x, float)
+        return Complex(self.re + x, self.im)
 
     def __mul__(self, other):
         real_part = self.re * other.re - self.im * other.im
         im_part = self.re * other.im + self.im * other.re
         return Complex(real_part, im_part)
 
-    def __rmul__(self, other):
-        pass
+    def __rmul__(self, x):
+        assert isinstance(x, float) 
+        return Complex(x*self.re, x*self.im)
 
     def __neg__(self):
         return Complex(-self.re, -self.im)
 
     def __sub__(self, other):
-        return self.__add__(Complex.neg(other))
+        return self + -other 
 
     def conjugate(self):
         return Complex(self.re, -self.im)
     
     @staticmethod
     def abs(self):
-        pass
+        """ Returns the absolute value of a complex number. Unsure whether to make this return a complex number or just a float."""
+        return self.re**2 + self.im**2 
 
     def inv(self):
         pass
